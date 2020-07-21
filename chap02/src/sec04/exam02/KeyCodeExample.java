a5b557b0a5904002ddcfd5269cccf3b4c2c5fde1
334-18\334-02 <lienero@naver.com>
2020-07-21 15:17:58 +0900

    0721

---
+++ chap02/src/sec04/exam02/KeyCodeExample.java
@@ -0,0 +1,22 @@
+package sec04.exam02;
+
+import java.io.IOException;
+
+public class KeyCodeExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int keyCode;
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode" + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode" + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode" + keyCode);
+
+	}
+
+}
